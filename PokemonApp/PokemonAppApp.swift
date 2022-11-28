62f42a033ca3f9183bd49bbf5f4991e759784bcf
randeep m <randeepu3@gmail.com>
2022-11-28 11:44:46 +0530

    Initial Commit

---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by randeep m.  on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
